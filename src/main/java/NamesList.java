10230d1fb4a3a786b7e78a49730bfbef6cba4445
Gretchen-z <rysrit@gmail.com>
2022-06-20 17:57:19 +0300

    исправление кодировки(1)

--- src/main/java/NamesList.java
+++ src/main/java/NamesList.java
@@ -1,7 +1,10 @@
-import java.io.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.http.*;
 
 public class NamesList extends HttpServlet {
     List<String> namesList = new ArrayList<>(){{
@@ -14,6 +17,7 @@ public class NamesList extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
         writer.print(namesList);
         writer.flush();
